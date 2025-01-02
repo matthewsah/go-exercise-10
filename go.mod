8cd490bf376678e680bac5c038446f393419db3b
Matthew Sah <mattsah2001@gmail.com>
2025-01-02 12:22:03 +0800

    created add function in add package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/matthewsah/go-exercise-10
+
+go 1.23.3
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/matthewsah/go-exercise-10/add"
+)
+
+func main() {
+	fmt.Println("hello")
+	fmt.Println(add.Add(1, 2))
+}
