ac24628a655693cf7c0271265f9fc4862c56ba7b
Matthew Sah <mattsah2001@gmail.com>
2025-01-02 18:28:02 +0800

    add godoc comments

--- add/add.go
+++ add/add.go
@@ -1,5 +1,10 @@
+// Package add is a package that contains the mathematical
+// function Add which adds two integers together
+// in the future there could be more adding functions which
+// could add numbers of different types
 package add
 
+// Add takes in two numbers and returns their sum
 func Add(num1 int, num2 int) int {
 	return num1 + num2
 }
